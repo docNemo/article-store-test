4e201d212c24da7500099971f9d26ab94c0c7dcf
docNemo <varyag300300@yandex.ru>
2023-04-26 20:09:45 +0300

    test checks

--- src/main/java/ru/mai/articlestore/configuration/StubConfiguration.java
+++ src/main/java/ru/mai/articlestore/configuration/StubConfiguration.java
@@ -1,6 +1,6 @@
 package ru.mai.articlestore.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.mai.articlestore.service.stub.StubService;
